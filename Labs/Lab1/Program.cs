9dd2101839fdd4e072a338fb550500a80f9f5c60
dkoval <dkoval@tsukat.com>
2021-09-05 23:38:17 +0300

    lab 1 project

---
+++ Labs/Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array =
+                Array.ConvertAll(Console.ReadLine()?.Trim().Split(' ') ?? Array.Empty<string>(), Convert.ToInt32)
+                    .Select(x => (IComparable)x);
+
+            var sortResult = MergeSort.Sort(array);
+            Console.WriteLine("Merging sort \n" +
+                              $"result: {sortResult.Result.Aggregate("", (s, comparable) => s + " " + comparable)} \n" +
+                              $"execution time: {sortResult.Time} \n" + 
+                              $"comparisons: {sortResult.Comparisons} \n" +
+                              $"swaps: {sortResult.Swaps}");
+            
+            Console.ReadKey();
+        }
+    }
+}
