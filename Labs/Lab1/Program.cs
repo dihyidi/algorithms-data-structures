252d22445b9763c2efb41d08057434632140ec5d
dihyidi <dn.koval3@gmail.com>
2021-09-13 22:10:13 +0300

    merge

---
+++ Labs/Lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enum.TryParse(args[0].ToUpper(), out OrderBy order);
+            
+            var array =
+                Array.ConvertAll(args[1].Trim().Split(','), Convert.ToInt32)
+                    .Select(x => (IComparable)x);
+
+            var sortResult = MergeSort.Sort(array, order);
+            Console.WriteLine("Merging sort \n" +
+                              $"result: {sortResult.Result.Aggregate("", (s, comparable) => s + " " + comparable)} \n" +
+                              $"execution time: {sortResult.Time} \n" + 
+                              $"comparisons: {sortResult.Comparisons} \n" +
+                              $"swaps: {sortResult.Swaps}");
+            
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# algorithms-data-structures
+**Laboratory 1**
+
+Your task is to implement a sorting algorithm. 
+
+The input data is a one-dimensional array of numbers, which is written through a comma and can be run as follows:
+python merge_sort.py asc 1,2,56,45, -9,78,11
+java MergeSort.java desc 1,2,56,45, -9,78,11
+asc - ascending
+desc - descending
+That is, your code must be able to sort ascending and descending.
+
+The result should display:
+* the name of the algorithm
+* work time
+* the number of comparison operations that were performed during the operation of the algorithm
+* the number of exchange operations that were performed during the operation of the algorithm
+* sorting results
+
+**How to run:**
+1. Clone the repo. (git clone *url*)
+2. Open cmd and go to the folder containing **Lab1.csproj**.
+3. Run **dotnet run [input params]**.
