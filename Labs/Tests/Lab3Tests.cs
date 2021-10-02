a1ac3d840119564b3dc47cea409d1e52dc35e527
dihyidi <dn.koval3@gmail.com>
2021-10-02 15:38:18 +0300

    tests

---
+++ Labs/Tests/Lab3Tests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Lab3;
+using Xunit;
+
+namespace Tests
+{
+    public class Lab3Tests
+    {
+        [Fact]
+        public void DiscountCounterCount_ReturnsMinSum()
+        {
+            //arrange
+            var prices = new List<int> { 10, 9, 7, 8, 3, 2, 1, 4, 5, 6 };
+            var discount = 40;
+
+            var expected = 38.8;
+            
+            //act
+            var actual = DiscountCounter.Count(prices, discount);
+            
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
