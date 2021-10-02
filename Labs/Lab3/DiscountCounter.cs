e15ebd42bf8996ea2c0f880bc4c24427ad03dabc
dihyidi <dn.koval3@gmail.com>
2021-10-02 15:30:12 +0300

    lab-5

---
+++ Labs/Lab3/DiscountCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Lab1;
+
+namespace Lab3
+{
+    public static class DiscountCounter
+    {
+        public static double Count(List<int> prices, int discount)
+        {
+            var third = prices.Count / 3;
+
+            var sortedPrices = MergeSort.Sort(prices.Select(x => (IComparable)x), OrderBy.ASC).Result
+                .Select(x => (int)x).ToArray();
+
+            var discountedPrices = sortedPrices
+                .TakeLast(third)
+                .Select(x => x * (0.01 * discount));
+
+            var sum = sortedPrices
+                          .SkipLast(third).Sum(x => x)
+                      + discountedPrices.Sum();
+
+            return sum;
+        }
+    }
+}
