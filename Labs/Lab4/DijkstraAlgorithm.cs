529d8f5b7eee15de626b9a6a5eaace2f9a93f009
dihyidi <dn.koval3@gmail.com>
2021-10-17 00:36:45 +0300

    dijksrta

---
+++ Labs/Lab4/DijkstraAlgorithm.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+
+namespace Lab4
+{
+    public static class DijkstraAlgorithm
+    {
+        private static int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
+        {
+            var min = int.MaxValue;
+            var minIndex = 0;
+
+            for (var v = 0; v < verticesCount; ++v)
+                if (!shortestPathTreeSet[v] && distance[v] <= min)
+                {
+                    min = distance[v];
+                    minIndex = v;
+                }
+
+            return minIndex;
+        }
+
+        public static double Dijkstra(int[,] graph, int source, int verticesCount)
+        {
+            var distance = new int[verticesCount];
+            var shortestPathTreeSet = new bool[verticesCount];
+
+            for (var i = 0; i < verticesCount; ++i)
+            {
+                distance[i] = int.MaxValue;
+                shortestPathTreeSet[i] = false;
+            }
+
+            distance[source] = 0;
+
+            for (var count = 0; count < verticesCount; ++count)
+            {
+                var u = MinimumDistance(distance, shortestPathTreeSet, verticesCount);
+                shortestPathTreeSet[u] = true;
+
+                for (var v = 0; v < verticesCount; ++v)
+                    if (!shortestPathTreeSet[v] && graph[u, v] > 0 && distance[u] != int.MaxValue &&
+                        distance[u] + graph[u, v] < distance[v])
+                        distance[v] = distance[u] + graph[u, v];
+            }
+
+            var distances = distance
+                .Where(x => x != int.MaxValue && x > 0)
+                .Select(x => (double)x);
+            
+            return distances.Sum() / distances.Count();
+        }
+    }
+}
---
+++ Labs/Lab4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Lab4
+{
+    internal static class Program
+    {
+        private const string BasePath = "C:/Users/dnkov/tazasho/3/algo/Labs/Lab4/";
+
+        public static void Main(string[] args)
+        {
+            var data = ReadGraphMatrix($"{BasePath}dijkstra_in.txt");
+            Console.WriteLine(DijkstraAlgorithm.Dijkstra(data.Item1, data.Item2, data.Item3));
+        }
+
+        private static (int[,], int, int) ReadGraphMatrix(string path)
+        {
+            var lines = File.ReadLines(path).ToArray();
+            
+            var firstLine = lines[0].Split(' ');
+            var verticesCount = int.Parse(firstLine[0]);
+            var source = int.Parse(firstLine[1]);
+
+            var graph = new int[verticesCount, verticesCount];
+
+            for (var i = 1; i < lines.Length; i++)
+            {
+                var elements = lines[i].Split(' ');
+                var row = int.Parse(elements[0]);
+                var column = int.Parse(elements[1]);
+                var cost = int.Parse(elements[2]);
+
+                graph[row, column] = cost;
+            }
+
+            return (graph, source, verticesCount);
+        }
+    }
+}
