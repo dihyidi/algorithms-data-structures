a2d3e250d365fc2d6cce6446bc4f75fa6b26df03
dihyidi <dn.koval3@gmail.com>
2021-11-14 16:16:17 +0200

    lab-6

---
+++ Labs/Lab6/BinarySeparator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab6
+{
+    public static class BinarySeparator
+    {
+        public static int FindSolution(string binaryString, int number)
+            => FindQuantityOfBinaryPows(binaryString, FindNumberSquares(binaryString, number));
+        
+        private static IEnumerable<string> FindNumberSquares(string binaryString, int number)
+        {
+            var squares = new List<string>();
+            var binaryNum = string.Empty;
+
+            for (var i = 0; binaryNum.Length < binaryString.Length; i++)
+            {
+                binaryNum = Convert.ToString((int) Math.Pow(number, i), 2);
+                squares.Add(binaryNum);
+            }
+            return squares;
+        }
+
+        private static int FindQuantityOfBinaryPows(string binaryString, IEnumerable<string> squares)
+        {
+            var res = 0;
+            var currentBinary = new StringBuilder();
+            
+            foreach (var ch in binaryString)
+            {
+                currentBinary.Append(ch);
+                for (var j = 0; j < squares.Count(); j++)
+                {
+                    var currentBinaryStr = currentBinary.ToString();
+                    if (!currentBinaryStr.Equals(squares.ElementAt(j)) || currentBinaryStr.Equals("1")) 
+                        continue;
+                    res++;
+                    currentBinary.Clear();
+                }
+            }
+            if (currentBinary.ToString().Equals("1"))
+                res++;
+            
+            return res > 0 ? res : -1;
+        }
+        
+    }
+}
---
+++ Labs/Lab6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BinarySeparator.FindSolution("110011011", 5));
+        }
+    }
+}
