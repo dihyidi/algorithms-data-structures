24e62bc3b5e240b036d7c99b08294666a7407f2c
dihyidi <dn.koval3@gmail.com>
2021-12-01 15:01:50 +0200

    lab 7

---
+++ Labs/Lab7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab7
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string txt = "AABAACAADAABAABA";
+            const string pat = "AABA";
+            Console.WriteLine("Found matches at index");
+            foreach (var item in KmpSearcher.Search(pat, txt))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Labs/Tests/Lab7Tests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Lab7;
+using Xunit;
+
+namespace Tests
+{
+    public class Lab7Tests
+    {
+        [Fact]
+        public void KmpSearch_ValidData_ReturnsListOfIndexes()
+        {
+            const string txt = "AABAACAADAABAABA";
+            const string pat = "AABA";
+            var expected = new List<int>() { 0, 9, 12 };
+
+            var actual = KmpSearcher.Search(pat, txt);
+            
+            Assert.Equal(expected, actual);
+        }
+        
+        [Fact]
+        public void KmpSearch_NoMatches_ReturnsEmptyList()
+        {
+            const string txt = "AABAACAADAABAABA";
+            const string pat = "Z";
+
+            var actual = KmpSearcher.Search(pat, txt);
+            
+            Assert.Empty(actual);
+        }
+    }
+}
